6625d534965c79fa55d807eb7c9a24abc200c2c4
Surender Pratap Singh <rk31151@gmail.com>
2024-12-19 17:25:31 +0530

    database modeling done

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -10,11 +10,48 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+const generateSignature = (paramsToSign) => {
+    console.log(paramsToSign, "paramsToSign");
+    const { api_secret } = cloudinary.config();
+    const sortedParams = Object.keys(paramsToSign).sort();
+    console.log(sortedParams, "sortedParams");
+    const paramsString = sortedParams
+        .map((key) => `${key}=${paramsToSign[key]}`)
+        .join("&");
+
+    console.log(paramsString, "paramsString");
+
+    const signature = crypto
+        .createHash("sha1")
+        .update(paramsString + api_secret)
+        .digest("hex");
+
+    return signature;
+};
+
 const uploadToCloudinary = async (filepath) => {
     try {
+        //explaination of the timestamp with example- 1631530000
+        const timeStamp = Math.round(new Date().getTime() / 1000);
+
+        // Parameters to sign include the timestamp
+        const paramsToSign = {
+            timestamp: timeStamp, // Include timestamp here
+        };
+
+        const signature = generateSignature(paramsToSign);
+
+        console.log(signature, "signature");
+
+        // Upload the file to cloudinary
         const result = await cloudinary.uploader.upload(filepath, {
-            resource_type: "auto",
+            ...paramsToSign, // Include timestamp in the upload request
+            signature, // Include the signature generated above
+            api_key:
+                process.env.CLOUDINARY_API_KEY || cloudinary.config().api_key,
         });
+
+        console.log(result, "upload result");
 
         return result;
     } catch (error) {
